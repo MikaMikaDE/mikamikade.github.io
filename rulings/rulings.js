8de5b169835a27b0e1b43a8c7f029dcbb696a1ff
MikaMikaDE <mika.k.pearson@Gmail.com>
2023-08-12 14:39:51 +0200

    1st rulings embed ver

--- rulings/rulings.js
+++ rulings/rulings.js
@@ -1,9 +1,9 @@
 function searchBar() {
     $.getJSON("./MikaRulings.json", function(data){
-        var data = data;
-        var userInput = document.getElementById("searchBar").value.toLowerCase();   
+        var userInput = document.getElementById("searchBar").value.toLowerCase();
         var filteredData = data.filter(val => val.CardName.toLowerCase() == userInput);
-        document.getElementById("resp").innerHTML = "result!"
-        
+        document.getElementById("cardName").innerHTML   = filteredData[0].CardName;
+        document.getElementById("psct").innerHTML       = filteredData[0].PSCT_Edison;
+        document.getElementById("rulings").innerHTML    = filteredData[0].Rulings_Edison;
     });
 }
